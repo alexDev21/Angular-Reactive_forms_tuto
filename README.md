c763d4a428aa7ba1b7edac4f47dc579c5ffca724
alexDev21 <alex2120058@gmail.com>
2022-05-26 18:28:54 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-zb9z5r
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-zb9z5r)
---
+++ src/app/profile-editor/profile-editor.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { FormArray } from '@angular/forms';
+
+@Component({
+  selector: 'app-profile-editor',
+  templateUrl: './profile-editor.component.html',
+  styleUrls: ['./profile-editor.component.css'],
+})
+export class ProfileEditorComponent {
+  profileForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: [''],
+    }),
+    aliases: this.fb.array([
+      this.fb.control(''), //NOTA: para q haya almenos un control de Alias x defecto, si no se necesita poner un control x defecto solo borrar esta linea.
+    ]),
+  });
+
+  get aliases() {
+    return this.profileForm.get('aliases') as FormArray;
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Nancy',
+      address: {
+        street: '123 Drew Street',
+      },
+    });
+  }
+
+  addAlias() {
+    this.aliases.push(this.fb.control(''));
+  }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
